de3c783223cb3547addfada8ac1c729b1399e7e7
BGQM <128929031+BGQM@users.noreply.github.com>
2023-07-24 12:10:13 -0400

    Add files via upload

--- Dash-Elect-App.py
+++ Dash-Elect-App.py
@@ -1,14 +1,17 @@
-#This works great!!!
-
 import pandas as pd
 import datetime as dt
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+import dash_bootstrap_components as dbc
+from pandas_ods_reader import read_ods
 
-colors = {'background': '#008B8B','text': '#FFF8DC'}
+colors = {'background': '#0086b3','text': '#ffffff'}#blue background and white text - Note: This matches the YETI theme!
 
-#Incorporate Data
+app = Dash(__name__, external_stylesheets=[dbc.themes.YETI])
+server = app.server
+
 df = pd.read_csv("https://raw.githubusercontent.com/BGQM/fpl/main/FPL_Bills.csv")
+#df = read_ods('/home/wg/Documents/WG Personal/FPL_Bills.ods')#this works, but only locally
 df['Date'] = pd.to_datetime(df['Date']) #Note: this is required to make into a datetime object
 df = df.set_index('Date')
 df['Month'] = df.index.month_name()
@@ -16,28 +19,24 @@ df['Year'] = df.index.year
 year_list = sorted(df['Year'].unique())#Note: sorting is probably not necessary
 max_year = df['Year'].max()
 
-#Initialize app
-app = Dash(__name__)
-server = app.server
-
 app.layout = html.Div([
-    html.H1('Avalon Electricity Usage and Cost',
-                         style={'fontSize':28, 
-                         'fontWeight' : 'bold',
-                         'textAlign':'center', 
-                         'color' : colors['text'],
-                         'background-color' : colors['background']}),
-    html.Hr(),#puts in a line
-    dcc.RadioItems(id='selected_year', options=year_list, value=max_year, inline=True),
+    dcc.Markdown('#### Avalon Electricity Usage',
+            style={'fontSize':24, 
+                    'fontWeight' : 'bold',
+                    'textAlign':'center', 
+                    'color' : colors['text'],
+                    'background-color' : colors['background']
+                    }),
+    html.Hr(),
+    
+    dcc.RadioItems(id='selected_year', options=year_list, value=max_year, inline=True,
+        labelStyle={'display':'inline-block','padding':'50','margin-left': '10px'}),
     html.Hr(),
     html.Button('Select kWh/Cost', n_clicks=0, id='button'),
     dcc.Graph(id="graph"),
     html.Hr(),
-    #html.H1('Indicator gauges here...')
-    #dcc.Markdown(id='text_displayed')
 ])
 
-#Initial populate chart
 @app.callback(
     Output("graph", "figure"), 
     Input("selected_year", "value"),
@@ -45,18 +44,17 @@ app.layout = html.Div([
     )
 
 def display_graph(value,n_clicks):
-    df2 = df.loc[str(value)]
+    dff = df.loc[str(value)]
+    avg_kWh = round(dff['Total'].sum()/dff['kWh'].sum(),3)
     if n_clicks % 2 == 0:
-        x, y = df2['Month'], df2['kWh']
+        x, y = dff['Month'], dff['kWh']
+        chart_title = str(dff['kWh'].sum()) + " kWh"
     else:
-        x, y = df2['Month'], df2['Total']
-
-    fig = px.bar(df2, x=x, y=y, text_auto=True, title=str(value) + " Avalon Monthly Electricity Usage/Cost") 
-    #print(value)#this is just for monitoring
-    #print(n_clicks)#this is just for monitoring
-    #print(df2)   
+        x, y = dff['Month'], dff['Total']
+        chart_title = str(dff['Total'].sum()) + " ($)"
+    fig = px.bar(dff, x=x, y=y, text_auto=True, title='Total Usage/Cost = ' + chart_title + "  --  Average Price: $/" + str(avg_kWh))  
     return fig
 
-#Run app
-if __name__ == '__main__':
-    app.run(debug=True)
+    #Run app
+if __name__ == "__main__":
+    app.run(debug=False)
