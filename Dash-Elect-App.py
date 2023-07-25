40026b34e44b329d20143f608a7cbc01061f5b29
BGQM <128929031+BGQM@users.noreply.github.com>
2023-07-25 14:45:40 -0400

    Add files via upload

--- Dash-Elect-App.py
+++ Dash-Elect-App.py
@@ -3,7 +3,6 @@ import datetime as dt
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import dash_bootstrap_components as dbc
-#from pandas_ods_reader import read_ods
 
 colors = {'background': '#0086b3','text': '#ffffff'}#blue background and white text - Note: This matches the YETI theme!
 
@@ -11,12 +10,11 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.YETI])
 server = app.server
 
 df = pd.read_csv("https://raw.githubusercontent.com/BGQM/fpl/main/FPL_Bills.csv")
-#df = read_ods('/home/wg/Documents/WG Personal/FPL_Bills.ods')#this works, but only locally
-df['Date'] = pd.to_datetime(df['Date']) #Note: this is required to make into a datetime object
+df['Date'] = pd.to_datetime(df['Date'])
 df = df.set_index('Date')
 df['Month'] = df.index.month_name()
 df['Year'] = df.index.year
-year_list = sorted(df['Year'].unique())#Note: sorting is probably not necessary
+year_list = sorted(df['Year'].unique())
 max_year = df['Year'].max()
 
 app.layout = html.Div([
@@ -45,14 +43,17 @@ app.layout = html.Div([
 
 def display_graph(value,n_clicks):
     dff = df.loc[str(value)]
-    avg_kWh = round(dff['Total'].sum()/dff['kWh'].sum(),3)
+    total_kWh = round((dff['kWh'].sum()),0)
+    total_cost = round((dff['Total'].sum()),2)
+    avg_kWh = round((dff['Total'].sum()/dff['kWh'].sum()),3)
+    
     if n_clicks % 2 == 0:
         x, y = dff['Month'], dff['kWh']
-        chart_title = str(dff['kWh'].sum()) + " kWh"
+        chart_title = str(value) + '   Total  ' + str(total_kWh) + ' kWh    ($' + str(avg_kWh) + '/kWh)'
     else:
         x, y = dff['Month'], dff['Total']
-        chart_title = str(dff['Total'].sum()) + " ($)"
-    fig = px.bar(dff, x=x, y=y, text_auto=True, title='Total Usage/Cost = ' + chart_title + "  --  Average Price: $/" + str(avg_kWh))  
+        chart_title = str(value) + '   Total  $' + str(total_cost) + '    ($' + str(avg_kWh) + ' /kWh)'
+    fig = px.bar(dff, x=x, y=y, text_auto=True, title= chart_title)  
     return fig
 
     #Run app
