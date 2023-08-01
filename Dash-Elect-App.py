a3b90c552927dc118960636421a174fe6e72f357
BGQM <128929031+BGQM@users.noreply.github.com>
2023-08-01 14:47:16 -0400

    Add files via upload

--- Dash-Elect-App.py
+++ Dash-Elect-App.py
@@ -18,7 +18,7 @@ year_list = sorted(df['Year'].unique())
 max_year = df['Year'].max()
 
 app.layout = html.Div([
-    dcc.Markdown('#### Avalon Electricity Usage',
+    dcc.Markdown('Avalon Electricity Usage',
             style={'fontSize':24, 
                     'fontWeight' : 'bold',
                     'textAlign':'center', 
@@ -26,23 +26,27 @@ app.layout = html.Div([
                     'background-color' : colors['background']
                     }),
     html.Hr(),
-    
-    dbc.RadioItems(id='selected_year', options=year_list, value=max_year, inline=True,
-        labelStyle={'display':'inline-block','marginRight': '10px'}),
+    dbc.RadioItems(id='selected_year', options=year_list, value=max_year, inline=True, 
+        labelStyle={'display':'inline-block','margin-right': '15px'}),
     html.Hr(),
-    dbc.Button('Select kWh/Cost', n_clicks=0, id='button', 
-    style={'marginLeft':'80px'}),
+    dbc.Button('Select kWh/Cost', n_clicks=0, id='button',  
+    style={'margin-left':'80px'}),
+    dcc.Markdown(id='chart_title',
+            style={'fontSize':24, 
+                    'fontWeight' : 'bold',
+                    'textAlign':'center'
+                    }),
     dcc.Graph(id="graph"),
     html.Hr(),
 ])
 
 @app.callback(
-    Output("graph", "figure"), 
+    Output("graph", "figure"),
+    Output('chart_title', 'children'), 
     Input("selected_year", "value"),
     Input("button", "n_clicks")
     )
-
-def display_graph(value,n_clicks):
+def display_graph(value, n_clicks):
     dff = df.loc[str(value)]
     total_kWh = round((dff['kWh'].sum()),0)
     total_cost = round((dff['Total'].sum()),2)
@@ -50,12 +54,13 @@ def display_graph(value,n_clicks):
     
     if n_clicks % 2 == 0:
         x, y = dff['Month'], dff['kWh']
-        chart_title = str(value) + '   Total  ' + str(total_kWh) + ' kWh    ($' + str(avg_kWh) + '/kWh)'
+        chart_title = str(value) + '   ---   Total  ' + str(total_kWh) + ' kWh   ---   $' + str(avg_kWh) + '/kWh'
     else:
         x, y = dff['Month'], dff['Total']
-        chart_title = str(value) + '   Total  $' + str(total_cost) + '    ($' + str(avg_kWh) + ' /kWh)'
-    fig = px.bar(dff, x=x, y=y, text_auto=True, title= chart_title)  
-    return fig
+        chart_title = str(value) + '   ---   Total  $' + str(total_cost) + '   ---   $' + str(avg_kWh) + '/kWh'
+    fig = px.bar(dff, x=x, y=y, text_auto=True
+    )  
+    return fig, chart_title
 
     #Run app
 if __name__ == "__main__":
